562c372dd82f8915b42b80e11c9661052e786cca
ZhangYixiSucceed <guoqingzhang0813@163.com>
2024-05-01 00:40:25 +0800

    add single list interface

--- DeviceControl/list.c
+++ DeviceControl/list.c
@@ -6,5 +6,33 @@
 
 void list_init()
 {
-    
+
+}
+
+
+void list_push_back()
+{
+
+}
+
+void list_push_front()
+{
+
+}
+
+
+
+void* list_pop()
+{
+
+}
+
+void list_remove()
+{
+
+}
+
+void list_erase()
+{
+
 }
---
+++ DeviceControl/list.h
@@ -0,0 +1,13 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+
+typedef struct list_struct
+{
+    list_t *node;
+    int count;
+}list_t; 
+
+
+#endif
+
